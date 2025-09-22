6a18b3f6a39a6085878fecd65fa943361d0cab48
Simon Silverstein <56710751+simonmso@users.noreply.github.com>
2025-09-22 13:12:34 +0200

    part c first plot

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
-from src.parts import A
+from src.parts import A, B, C
 
 plt.rcParams.update(
     {
         "figure.dpi": 600,
         "figure.constrained_layout.use": True,
         "legend.scatterpoints": 4,
+        # "savefig.format": "svg",
     }
 )
 
-A.main()
+# A.main()
+# B.main()
+C.main()
--- Project_1/src/regression.py
+++ Project_1/src/regression.py
@@ -4,3 +4,7 @@ import numpy as np
 def OLS(X, y):
     # TODO: catch non-invertable
     return np.linalg.inv(X.T @ X) @ X.T @ y
+
+
+def ridge(X, y, lam=1e-3):
+    return np.linalg.inv(X.T @ X + lam * np.identity(X.shape[1])) @ X.T @ y
