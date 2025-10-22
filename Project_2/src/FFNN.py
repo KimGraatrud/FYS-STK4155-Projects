be75b2d83263c4fab84a1bcca3f230bd1b1e5444
Kim G <kim.davidsen2003@gmail.com>
2025-10-22 15:47:03 +0200

    outlined project 2

---
+++ Project_2/src/FFNN.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class FFNN:
+
+    def __init__(        
+        self,
+        network_input_size,
+        layer_output_sizes,
+        activation_funcs,
+        activation_ders,
+        cost_fun,
+        k,
+        cost_der,
+):
+        pass
+
+    def cost(self, inputs, targets):
+        pass
+
+    def _feed_forward_saver(self, inputs):
+        pass
+
+    def compute_gradient(self, inputs, targets):
+        pass
+
+    def update_weights(self, layer_grads):
+        pass
+
+    def create_layers(self):
+        pass
+
