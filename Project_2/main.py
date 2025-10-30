67f9d83ff0c2b7d768e0c60d416593970cdf1ecf
Simon Silverstein <56710751+simonmso@users.noreply.github.com>
2025-10-30 11:20:16 +0100

    neet viz code

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,6 +1,15 @@
 import os
+import matplotlib.pyplot as plt
 from src import utils
 from parts import B, F
+
+plt.rcParams.update(
+    {
+        # Figure
+        "figure.dpi": 600,
+        "figure.constrained_layout.use": True,
+    }
+)
 
 # create a home for figures
 if not os.path.exists(utils.FIGURES_URL):
--- Project_2/parts/F.py
+++ Project_2/parts/F.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from src.FFNN import FFNN
 from src.ClassifierNN import ClassifierNN
 from src import utils
@@ -27,11 +28,17 @@ def main():
         network_input_size=X_train.shape[0],
         classes=y_train.shape[0],
         layer_output_sizes=[
-            25,
-            15,
+            # 10,
+            # 15,
         ],
-        activation_funcs=[utils.ReLU, utils.ReLU],
-        activation_ders=[utils.ReLU_der, utils.ReLU_der],
+        activation_funcs=[
+            # utils.ReLU,
+            # utils.ReLU,
+        ],
+        activation_ders=[
+            # utils.ReLU_der,
+            # utils.ReLU_der,
+        ],
         eta=1e-3,
         batch_size=150,
     )
@@ -43,7 +50,40 @@ def main():
             test = utils.accuracy(nn(X_test), y_test)
             print(f"train: {train:.4f} test: {test:.4f} iter: {i}")
 
-    nn.train(X_train, y_train, n_iter=1e6, callback=train_callback)
+            # W, b = nn.layers[0]
+            # fig, axs = plt.subplots(nrows=W.shape[1], figsize=(4, 16))
+            # for n in range(W.shape[1]):
+            #     ax = axs[n]
+            #     ax.imshow(W[:, n].reshape(28, 28))
+
+            # fig.savefig(os.path.join(utils.FIGURES_URL, f"layer_{i}"))
+
+            # plt.close(fig)
+
+    nn.train(X_train, y_train, n_iter=2001, callback=train_callback)
+
+    pred = nn(X_test)
+    norm = mpl.colors.Normalize(vmin=-0.75, vmax=0.75)
+    W, b = nn.layers[0]
+    for i in range(7):
+        fig, axs = plt.subplots(nrows=W.shape[1], ncols=3, figsize=(8, 16))
+
+        inp = X_test[:, i]
+        out = pred[:, i]
+
+        axs[0][0].imshow(inp.reshape(28, 28), norm=norm)
+
+        for n in range(W.shape[1]):
+            axs[n][1].imshow(W[:, n].reshape(28, 28), norm=norm)
+            result = W[:, n] * inp
+            # result = result / np.mean(result) - 1
+            axs[n][2].imshow(result.reshape(28, 28))
+            axs[n][2].set_ylabel(f"{out[n]:.2f}")
+            if n != 0:
+                fig.delaxes(axs[n][0])
+
+        fig.savefig(os.path.join(utils.FIGURES_URL, f"result_{i}"))
+        plt.close(fig)
 
 
 if __name__ == "__main__":
